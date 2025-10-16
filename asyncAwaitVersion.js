a02d37e9a9eff5aeb80522d3be35f2c6aacef5b9
Karen Enderle <kee.enderle@gmail.com>
2025-10-15 21:56:19 -0400

    Add asyncAwaitVersion.js using async/await syntax

---
+++ asyncAwaitVersion.js
@@ -0,0 +1,79 @@
+function mowYard(name) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`${name} finished mowing the yard`);
+      resolve();
+    }, 2000);
+  });
+}
+
+function weedEat(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const complete = Math.random() > 0.5;
+      if (complete) {
+        console.log(`${name} finished using the weedeater.`);
+        resolve();
+      } else {
+        reject(`${name} fell asleep after mowing the yard.`);
+      }
+    }, 1500);
+  })
+}
+
+function trimHedges(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const complete = Math.random() > 0.5;
+      if (complete) {
+        console.log(`${name} finished trimming the hedges.`);
+        resolve();
+      } else {
+        reject(`${name} fell asleep after weed eating the yard.`);
+      }
+    }, 1000);
+  });
+}
+
+function collectWood(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const complete = Math.random() > 0.5;
+      if (complete) {
+        console.log(`${name} finished collecting wood.`);
+        resolve();
+      } else {
+        reject(`${name} fell asleep after trimming the hedges.`);
+      }
+    }, 2500);
+  });
+}
+
+function waterGarden(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const complete = Math.random() > 0.5;
+      if (complete) {
+        console.log(`${name} finished watering the garden.`);
+        resolve();
+      } else {
+        reject(`${name} fell asleep after collecting wood.`);
+      }
+    }, 500);
+  });
+}
+
+async function doSummerChores(name) {
+    try {
+        await mowYard(name);
+        await weedEat(name);
+        await trimHedges(name);
+        await collectWood(name);
+        await waterGarden(name);
+        console.log(`${name} finished all their summer chores!`);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+doSummerChores('Karen');
